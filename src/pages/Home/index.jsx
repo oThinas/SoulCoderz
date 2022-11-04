fac7b90c1a55635f87ccfb1d368876427196c9fa
EnzoTrevisann <enzo.trev@hotmail.com>
2022-11-03 23:32:22 -0300

    alteração na estilização

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,16 +4,15 @@ import HomeImg3 from '../../assets/img/HomeImg3.png'
 import { Carousel } from '../../components/Carousel'
 export default function Home() {
 
-
   return (
     <section>
       <div class="header">
         <div class="container">
           <div class="row">
             <div class="col-2">
-              <h1>Tenha praticidade no cotidiano👋</h1>
-              <h2>É para todos!</h2>
-              <p class="sub-title">As bicicletas e patinetes são para todos, basta se cadastrar no site e ler o QR Code, <br /> De bike, São Paulo fica mais perto de você</p>
+              <h1>Tenha <span>praticidade</span> no cotidiano👋</h1>
+              <h2 className='title'>É para todos!</h2>
+              <p class="main_info">As bicicletas e patinetes são para todos, basta se cadastrar no site e ler o QR Code, <br /> De bike, São Paulo fica mais perto de você</p>
               <a href="#start" class="button">Veja Mais! &#8594;</a>
             </div>
             <div class="col-2">
@@ -24,10 +23,11 @@ export default function Home() {
       </div>
       <div className='categories'>
         <div className='small-container'>
-          <h2 className='title' id='start'>SELECIONE O TAMANHO <br />QUE MAIS COMBINA COM VOCÊ <br /> KIDS OU ADULTO </h2>
+          <h2 className='sub-title'>Selecione o tamanho<br />que mais <span>combina com você</span></h2>
+          <p class="info">KIDS OU ADULTO</p>
           <Carousel />
-          <h2 className='title' id='start'>Libere sua bike em 3 passos  </h2>
-          <h3 className='title' id='start'>1. Cadastre-se na plataforma <br /> 2. Aponte sua câmera para o QRCode presente em nossos pontos <br /> 3. Pronto. Sua bike está liberada e pronto para uso</h3>
+          <h2 className='sub-title'>Libere sua bike em <br /><span>apenas 3 passos</span></h2>
+          <h3 className='sub-title'>1. Cadastre-se na plataforma <br /> 2. Aponte sua câmera para o QRCode presente em nossos pontos <br /> 3. Pronto. Sua bike está liberada e pronto para uso</h3>
           <div className='row'>
             <div className='col-3'>
               <img src={HomeImg3} alt='Imagem de uma criança andando de bicicleta' />
