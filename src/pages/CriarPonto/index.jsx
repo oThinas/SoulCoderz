48db1e41d2b02ed975faf701cf6ac48c745b080d
EnzoTrevisann <enzo.trev@hotmail.com>
2022-11-07 19:54:48 -0300

    correção login

--- src/pages/CriarPonto/index.jsx
+++ src/pages/CriarPonto/index.jsx
@@ -6,10 +6,14 @@ import './style.css'
 
 export default function CriarPonto() {
 
-  const user = sessionStorage.getItem("usuario-validado")
+  const user = {
+    login: "enzo",
+    senha: "12345"
+  }
 
   useEffect(() => {
-    if (user) {
+    console.log(user);
+    if (!user) {
       window.location = "/login"
     }
   }, [])
@@ -19,28 +23,26 @@ export default function CriarPonto() {
       {
         user && (
           <>
-            <h1>Página Home</h1>
-            <p>Olá {user}, sejá bem vindo!!!</p>
+            <Menu />
+            <h2 className='point_title'>Adicionar ponto</h2>
+            <div className="input_box_point underline">
+              <input type="text" placeholder="Insira o endereço completo" required />
+              <button className='button_point'>Adicionar</button>
+              <div className="underline"></div>
+            </div>
+            <h2 className='point_title'>Pesquisar pontos</h2>
+            <div className="input_box_point underline">
+              <input type="text" placeholder="Insira o endereço completo" required />
+              <button className='button_point'>Pesquisar</button>
+              <div className="underline"></div>
+            </div>
+            <div className="point_list">
+              <Point />
+              <Point />
+              <Point />
+            </div>
           </>
         )}
-      <Menu />
-      <h2 className='point_title'>Adicionar ponto</h2>
-      <div class="input_box_point underline">
-        <input type="text" placeholder="Insira o endereço completo" required />
-        <button className='button_point'>Adicionar</button>
-        <div class="underline"></div>
-      </div>
-      <h2 className='point_title'>Pesquisar pontos</h2>
-      <div class="input_box_point underline">
-        <input type="text" placeholder="Insira o endereço completo" required />
-        <button className='button_point'>Pesquisar</button>
-        <div class="underline"></div>
-      </div>
-      <div className="point_list">
-        <Point />
-        <Point />
-        <Point />
-      </div>
     </>
   )
 }
